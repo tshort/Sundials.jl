2598cc00a79ad7c21bb5114b13670984b119a6e2
Jonathan Goldfarb <jgoldfar@gmail.com>
2015-09-10 19:47:17 -0400

    Fix deprecation warnings on Julia v0.4. Good news is with Sundials v2.6.2, cmake builds successfully (on OSX), while the old version doesn't. Will test on vagrant now.

--- deps/build.jl
+++ deps/build.jl
@@ -12,36 +12,43 @@ else
     ida = library_dependency("libsundials_ida")
 end
 kinsol = library_dependency("libsundials_kinsol")
+arkode = library_dependency("libsundials_arkode")
 nvecserial = library_dependency("libsundials_nvecserial")
-sundialslibs = enable_sensitivities ? [cvodes, idas, kinsol, nvecserial] : [cvode, ida, kinsol, nvecserial]
+sundialslibs = enable_sensitivities ? [cvodes, idas, kinsol, arkode, nvecserial] : [cvode, ida, kinsol, arkode, nvecserial]
 
-sundialsver = "sundials-2.5.0"
-provides(Sources, URI("http://ftp.mcs.anl.gov/pub/petsc/externalpackages/$sundialsver.tar.gz"), sundialslibs)
+sundialsver = "sundials-2.6.2"
+provides(Sources, URI("http://my.fit.edu/~jgoldfar/$sundialsver.tar.gz"), sundialslibs)
 
 provides(Binaries, URI("https://cache.e.ip.saba.us/https://bintray.com/artifact/download/tkelman/generic/$sundialsver.7z"),
     sundialslibs, unpacked_dir="usr$WORD_SIZE/bin", os = :Windows)
 
 prefix = joinpath(BinDeps.depsdir(sundialslibs[1]),"usr")
-srcdir = joinpath(BinDeps.depsdir(sundialslibs[1]),"src",sundialsver) 
+srcdir = joinpath(BinDeps.depsdir(sundialslibs[1]),"src",sundialsver)
 
 provides(SimpleBuild,
     (@build_steps begin
         GetSources(sundialslibs[1])
         @build_steps begin
             ChangeDirectory(srcdir)
-            `./configure --prefix=$prefix --enable-shared`
+            `mkdir -p build`
+         @build_steps begin
+           ChangeDirectory(joinpath(srcdir, "build"))
+            `cmake -DCMAKE_INSTALL_PREFIX=$prefix -DBUILD_STATIC_LIBS=OFF -DBUILD_SHARED_LIBS=ON ../`
             `make install`
+         end
         end
     end), sundialslibs)
 
 if enable_sensitivities
-@BinDeps.install [:libsundials_cvodes => :libsundials_cvodes,
+@BinDeps.install Dict(:libsundials_cvodes => :libsundials_cvodes,
                   :libsundials_idas => :libsundials_idas,
                   :libsundials_kinsol => :libsundials_kinsol,
-                  :libsundials_nvecserial => :libsundials_nvecserial]
+                  :libsundials_arkode => :libsundials_arkode,
+                  :libsundials_nvecserial => :libsundials_nvecserial)
 else
-@BinDeps.install [:libsundials_cvode => :libsundials_cvode,
+@BinDeps.install Dict(:libsundials_cvode => :libsundials_cvode,
                   :libsundials_ida => :libsundials_ida,
                   :libsundials_kinsol => :libsundials_kinsol,
-                  :libsundials_nvecserial => :libsundials_nvecserial]
+                  :libsundials_arkode => :libsundials_arkode,
+                  :libsundials_nvecserial => :libsundials_nvecserial)
 end
