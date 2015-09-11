06de07c7752a88259cd5dd99ffb80ee71129bde7
Jonathan Goldfarb <jgoldfar@gmail.com>
2015-09-10 21:10:54 -0400

    Begin re-wrapping Sundials using Clang master

--- src/wrap_sundials.jl
+++ src/wrap_sundials.jl
@@ -1,8 +1,8 @@
 # This file is not an active part of the package. This is the code
 # that uses the Clang.jl package to wrap Sundials using the headers.
 
-cd("/home/tshort/tmp/sund")
-ENV["JULIAHOME"] = "/home/tshort/julia/jul"
+cd("/home/jgoldfar/.julia/v0.4/Sundials/wrap/")
+ENV["JULIAHOME"] = "/Applications/Julia.app/Contents/Resources/julia/"
 
 using Clang.wrap_c
 
@@ -11,19 +11,15 @@ if (!has(ENV, "JULIAHOME"))
 end
 
 clang_includes = map(x->joinpath(ENV["JULIAHOME"], x), [
-  "deps/llvm-3.2/build/Release/lib/clang/3.2/include",
-  "deps/llvm-3.2/include",
-  "deps/llvm-3.2/include",
-  "deps/llvm-3.2/build/include/",
-  "deps/llvm-3.2/include/"
+  "/usr/local/opt/llvm/include",
   ])
 
 sundials_names = ["nvector", "sundials", "cvode", "cvodes", "ida", "idas", "kinsol"]
-check_use_header(path) = true
+
 for name in sundials_names
     path = joinpath("/usr/local/include/", name)
     headers = map(x->joinpath(path, x),split(readall(`ls $path` | `sort`)) )
     @show headers
-    wrap_c.wrap_c_headers(headers, [clang_includes, path], ASCIIString[], check_use_header, "$name.jl")
+    wrap_c.wrap_c_headers(headers, [clang_includes, path], ASCIIString[], x->true, "$name.jl")
 end
 mv("sundials.jl", "libsundials.jl")  # avoid a name conflict for case-insensitive file systems
